dc221c726a17adc1121aad33a9f29dad552f53d0
Jurek Weber <jurek@afrika2.de>
2022-06-28 12:01:07 +0200

    some little changes

--- include/fisher.h
+++ include/fisher.h
@@ -1,4 +1,7 @@
-#define MAX_FRAME_BUFFER_SIZE 100
+#include <stdint.h>
+
+#define MAX_FRAME_BUFFER_SIZE   100
+#define ADDRESS_SIZE            8
 
 
 typedef enum Status {
@@ -7,58 +10,69 @@ typedef enum Status {
 } Status;
 
 typedef enum fisher_frame_type {
-    HELLO,
+    OGM2,
 
 } fisherFrameType;
 typedef struct Address {
-
+    uint8_t Addr;
 } Address;
 
 
 // what should be serialized/deserialized and send/recieved via cc1200
 // gets build by the agorithm
-typedef struct fisher_frame {
+struct fisher_frame {
     int type;
+    /*
+     * OGM2
+     */
+    // TTL
+    int seq;
+    Address originator;
+    /*
+     * DATA Packet
+     */
     Address sender;
-    Address recipand;
+    Address recipient;
     int ttl;
     int rssi;
     int length;
     char * content;
-} fisherFrame;
+};
 
 // a DATA package that should be routed
-typedef struct fisher_package {
+struct fisher_package {
     Address sender;
     Address receiver;
     int ttl;
     int length;
     char * content;
-} fisherPackage;
+};
 
-typedef struct fisher_boat {
+struct fisher_boat {
     Address addr;
     // routing table
+    //struct fisher_route route[ADDRESS_SIZE];
     // routing
-    fisherFrame to_be_sent[MAX_FRAME_BUFFER_SIZE];
+    struct fisher_frame to_be_sent[MAX_FRAME_BUFFER_SIZE];
 
-} fisherBoat;
+};
 
 // inits the fisherBoat struct, sets address, inits clean routing table
 // #pre empty boat
 // #post boat contains address
-Status fisher_init(fisherBoat *boat, Address addr);
+Status fisher_init(struct fisher_boat *boat, Address addr);
 
 // gets executed evey loop, generate HELLO frames
 // #pre boat in old state
 // #post if algorithm decides to send hello messages,
-Status fisher_tick(fisherBoat *boat);
+Status fisher_tick(struct fisher_boat *boat);
 
 // generates data frames
-Status fisher_packet_generate(fisherBoat *boat /* TODO */);
-Status fisher_packet_read(fisherBoat* boat /* TODO */);
+Status fisher_packet_generate(struct fisher_boat *boat /* TODO */);
+Status fisher_packet_read(struct fisher_boat* boat /* TODO */);
 
 // responds to hello frames
 // routes frames
-Status fisher_frame_process(fisherBoat *boat, fisherFrame * frames, int len /* TODO */);
-Status fisher_frame_get_to_be_sent(fisherBoat *boat, fisherFrame * frames, int *len /* TODO */);
+// if hardware receives a packet, it will be passed to the algorithm with this function
+Status fisher_frame_process(struct fisher_boat *boat, struct fisher_frame * frames, int len /* TODO */);
+Status fisher_frame_get_to_be_sent(struct fisher_boat *boat, struct fisher_frame * frames, int *len /* TODO */);
--- src/fisher.c
+++ src/fisher.c
@@ -1,4 +1,38 @@
 //
 // Created by Jurek on 21.06.22.
 //
+#include "fisher.h"
 
+Status fisher_init(struct fisher_boat *boat, Address addr){
+    // init all with 0, create new routing table etc
+}
+
+// gets executed evey loop, generate HELLO frames
+// #pre boat in old state
+// #post if algorithm decides to send hello messages,
+Status fisher_tick(struct fisher_boat *boat) {
+    // TODO
+    // compare if time has passed to send ogm2 msg
+    // generate ogm2 msg
+    // send
+}
+
+// generates data frames
+Status fisher_packet_generate(struct fisher_boat *boat /* TODO */) {
+
+}
+Status fisher_packet_read(struct fisher_boat* boat /* TODO */) {
+    // TODO
+    // check if it is a ogm msg and forward
+}
+
+// responds to hello frames
+// routes frames
+// if harware recieves a packet, it will be passed to the algorithm with this function
+Status fisher_frame_process(struct fisher_boat *boat, struct fisher_frame * frames, int len /* TODO */) {
+
+}
+Status fisher_frame_get_to_be_sent(struct fisher_boat *boat, struct fisher_frame * frames, int *len /* TODO */) {
+    // TODO
+    // return the frames to be sendet
+}
