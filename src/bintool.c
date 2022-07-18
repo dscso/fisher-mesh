13af13e6b99fd849cd39564d1c3d34396fdf4b29
Jurek Weber <jurek@afrika2.de>
2022-07-18 12:42:04 +0200

    serialize/deserialize working now

--- src/bintool.c
+++ src/bintool.c
@@ -4,31 +4,25 @@
 
 int fisher_serialize(struct fisher_frame *frame, uint8_t* buffer) {
     int pos = 0;
-    buffer[pos] = (uint8_t) frame->type;
-    pos += 1;
+    buffer[pos++] = (uint8_t) frame->type;
 
-    buffer[pos] = (uint8_t) frame->originator;
-    pos += 1;
+    buffer[pos++] = (uint8_t) frame->originator;
 
-    buffer[pos] = (uint8_t) frame->sender;
-    pos += 1;
+    buffer[pos++] = (uint8_t) frame->sender;
 
-    buffer[pos] = (uint8_t) frame->receiver;
-    pos += 1;
+    buffer[pos++] = (uint8_t) frame->receiver;
 
-    buffer[pos] = (uint8_t) frame->recipient;
-    pos += 1;
+    buffer[pos++] = (uint8_t) frame->recipient;
 
-    buffer[pos] = (uint8_t) frame->hops;
-    pos += 1;
+    buffer[pos++] = (uint8_t) frame->hops;
 
     switch (frame->type) {
         case FISHER_FRAME_TYPE_HELLO:
-            buffer[pos] = (uint8_t) frame->seq;
+            buffer[pos++] = (uint8_t) frame->seq;
             break;
         case FISHER_FRAME_TYPE_DATA :
-            buffer[pos] = (uint8_t) frame->length;
-            pos++;
+            buffer[pos++] = (uint8_t) frame->length;
+
             memcpy(buffer + pos, frame->content, frame->length);
             pos += frame->length;
             break;
@@ -42,32 +36,25 @@ int fisher_serialize(struct fisher_frame *frame, uint8_t* buffer) {
 }
 
 int fisher_deserialize(struct fisher_frame *frame, uint8_t* buffer) {
-
     int pos = 0;
-    frame->type = buffer[pos];
-    pos += 1;
+    frame->type = buffer[pos++];
 
-    frame->originator = buffer[pos];
-    pos += 1;
+    frame->originator = buffer[pos++];
 
-    frame->sender = buffer[pos];
-    pos += 1;
+    frame->sender = buffer[pos++];
 
-    frame->receiver = buffer[pos];
-    pos += 1;
+    frame->receiver = buffer[pos++];
 
-    frame->recipient = buffer[pos];
-    pos += 1;
+    frame->recipient = buffer[pos++];
 
-    frame->hops = buffer[pos];
-    pos += 1;
+    frame->hops = buffer[pos++];
 
     switch (frame->type) {
         case FISHER_FRAME_TYPE_HELLO :
-            frame->seq = buffer[pos];
+            frame->seq = buffer[pos++];
             break;
         case FISHER_FRAME_TYPE_DATA:
-                frame->length = buffer[pos];
+                frame->length = buffer[pos++];
                 memcpy(frame->content, buffer + pos, frame->length);
                 break;
         default:
